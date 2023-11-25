1c81df141b6744b183d3e674e511e2a88ef2c5ea
Schin Ádám <schin.adam@gmail.com>
2023-11-25 10:48:07 +0100

    CSS refactor

--- public/functions/auth-result.js
+++ public/functions/auth-result.js
@@ -1,11 +1,15 @@
 const params = (new URL(document.location)).searchParams;
 const isSuccess = params.get("success");
 
-if (isSuccess === 'false') {
-    const resultImage = document.getElementById('result-img');
-    const resultText = document.getElementById('result-text');
-    const resultButton = document.getElementById('result-button');
+const resultImage = document.getElementById('result-img');
+const resultText = document.getElementById('result-text');
+const resultButton = document.getElementById('result-button');
 
+if (isSuccess === 'true') {
+    resultImage.src = "./assets/images/success-img.svg";
+    resultText.innerText = "Successful authentication!";
+    resultButton.innerText = "Done";
+} else {
     resultImage.src = "./assets/images/warning-img.svg";
     resultText.innerText = "Authentication failed!";
     resultButton.innerText = "Log out";
