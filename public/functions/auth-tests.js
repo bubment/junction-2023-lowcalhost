d0de18065f2d63f84827fb95cda2f1195be71abe
Schin Ádám <schin.adam@gmail.com>
2023-11-25 15:54:41 +0100

    added tests

---
+++ public/functions/auth-tests.js
@@ -0,0 +1,33 @@
+const testArray = [
+    {
+        question: "What flavour is the milk on the picture?",
+        answer: "chocolate",
+        asset: "./assets/images/greenwarning-img.svg"
+    },
+    {
+        question: "What flavour is the milk on the picture?",
+        answer: "strawberry",
+        asset: "./assets/images/greenwarning-img.svg"
+    },
+    {
+        question: "What color is the juice on the picture?",
+        answer: "orange",
+        asset: "./assets/images/greenwarning-img.svg"
+    },
+    {
+        question: "What color is the juice on the picture?",
+        answer: "green",
+        asset: "./assets/images/greenwarning-img.svg"
+    },
+    {
+        question: "How many fingers does the hand have on the picture?",
+        answer: "four",
+        asset: "./assets/images/greenwarning-img.svg"
+    },
+    {
+        question: "How many fingers does the hand have on the picture?",
+        answer: "seven",
+        asset: "./assets/images/greenwarning-img.svg"
+    },
+
+];
