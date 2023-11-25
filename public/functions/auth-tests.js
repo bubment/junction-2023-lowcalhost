2d508d8dbe2d0133a0fdd13d5afbbb4c8257efa9
Schin Ádám <schin.adam@gmail.com>
2023-11-25 16:50:02 +0100

    Update auth-tests.js

--- public/functions/auth-tests.js
+++ public/functions/auth-tests.js
@@ -1,33 +1,43 @@
 const testArray = [
     {
         question: "What flavour is the milk on the picture?",
-        answer: "chocolate",
-        asset: "./assets/images/greenwarning-img.svg"
+        answer: "chocholate",
+        asset: "./assets/images/chocholate-milk.svg"
     },
     {
         question: "What flavour is the milk on the picture?",
         answer: "strawberry",
-        asset: "./assets/images/greenwarning-img.svg"
+        asset: "./assets/images/strawberry-milk.svg"
     },
     {
         question: "What color is the juice on the picture?",
         answer: "orange",
-        asset: "./assets/images/greenwarning-img.svg"
+        asset: "./assets/images/orange-juice.svg"
     },
     {
         question: "What color is the juice on the picture?",
         answer: "green",
-        asset: "./assets/images/greenwarning-img.svg"
+        asset: "./assets/images/green-juice.svg"
     },
     {
         question: "How many fingers does the hand have on the picture?",
         answer: "four",
-        asset: "./assets/images/greenwarning-img.svg"
+        asset: "./assets/images/four-fingers.svg"
     },
     {
         question: "How many fingers does the hand have on the picture?",
         answer: "seven",
-        asset: "./assets/images/greenwarning-img.svg"
+        asset: "./assets/images/seven-fingers.svg"
+    },
+    {
+        question: "How many eggs are in the egg tray?",
+        answer: "nine",
+        asset: "./assets/images/nine-eggs.svg"
+    },
+    {
+        question: "How many eggs are in the egg tray?",
+        answer: "seven",
+        asset: "./assets/images/five-eggs.svg"
     },
 
 ];
