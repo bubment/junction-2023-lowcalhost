2c9745fb33ccf8bbe02c8ae8a33c288a97aba736
Medgyesi Attila <attila.medgyesi@goodgrind.tech>
2023-11-25 12:27:46 +0100

    Add voice recorder page instruction change

--- public/functions/voice-recorder.js
+++ public/functions/voice-recorder.js
@@ -1,6 +1,12 @@
 function startRecording() {
+    //Change the image in the record panel
     const recordPanel = document.getElementById("record-action-image")
     recordPanel.src = "../assets/images/record-voice.svg";
+    // Change the instruction text to sample text
+    const voiceRecorderInstruction = document.getElementById("voice-recorder-instruction")
+    voiceRecorderInstruction.innerHTML = "You only live once, but if you do it right, once is enough."
+    voiceRecorderInstruction.classList.add("font-bold")
+    // [TODO]
     //Record voice
     //Send to the server
     //in case of error restart the process
